0ace6baaaba473f11d17282515e1477aa5ce81f1
scherepanov <sergey.cherepanov@ewave.com>
2019-05-30 14:44:36 +0400

    mongodb@3.6

--- Formula/mongodb@3.6.rb
+++ Formula/mongodb@3.6.rb
@@ -16,7 +16,7 @@ class MongodbAT36 < Formula
 
   keg_only :versioned_formula
 
-  depends_on "go" => :build
+  depends_on "go@1.11" => :build
   depends_on "pkg-config" => :build
   depends_on "scons" => :build
   if OS.mac?
