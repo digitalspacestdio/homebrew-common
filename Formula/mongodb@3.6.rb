bababb50f046b1d90c1a04d0a8f1d8be12d61af6
scherepanov <sergey.cherepanov@ewave.com>
2019-05-30 17:03:29 +0400

    mongodb@3.6

--- Formula/mongodb@3.6.rb
+++ Formula/mongodb@3.6.rb
@@ -42,6 +42,7 @@ class MongodbAT36 < Formula
   end
 
   def install
+    ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
     ENV.libcxx
 
     ["Cheetah", "PyYAML", "typing"].each do |r|
