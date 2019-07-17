05c2ac3ef040c26fff380cb79daa5a24a66ec997
scherepanov <sergey.cherepanov@ewave.com>
2019-07-17 15:28:09 +0400

    icu4c@61

--- Formula/icu4c@61.rb
+++ Formula/icu4c@61.rb
@@ -19,7 +19,7 @@ class Icu4cAT61 < Formula
 
   def install
     args = %W[--prefix=#{prefix} --disable-samples --disable-tests --enable-static]
-    args << "--with-library-bits=64" if MacOS.prefer_64_bit?
+    #args << "--with-library-bits=64" if MacOS.prefer_64_bit?
 
     cd "source" do
       system "./configure", *args
