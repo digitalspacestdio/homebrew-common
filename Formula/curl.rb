48d364bc3458d472ff291c2aeaa682ffc9a5ee33
scherepanov <sergey.cherepanov@ewave.com>
2019-08-02 15:27:24 +0400

    curl

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -21,6 +21,7 @@ class Curl < Formula
   head do
     url "https://github.com/curl/curl.git"
 
+    depends_on "openssl"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
