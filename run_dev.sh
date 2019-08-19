79a63bfd5c5d4689187c76f75421fb8c7bc37008
scherepanov <sergey.cherepanov@ewave.com>
2019-08-19 19:41:25 +0400

    dev container, dockerfiles

--- run_dev.sh
+++ run_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-docker run -it -v ${SCRIPT_DIR}:/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/djocker/homebrew-common  djocker/linuxbrew bash
+docker run -it -e "HOMEBREW_NO_AUTO_UPDATE=1" -v ${SCRIPT_DIR}:/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/djocker/homebrew-common  djocker/linuxbrew bash
