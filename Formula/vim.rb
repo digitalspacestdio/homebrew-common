6259b1e64ca52f17baa1729158b27d90ff85757a
scherepanov <sergey@cherepanov.org.ua>
2020-05-28 19:25:30 +0400

    remote outdated bottles

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -6,11 +6,11 @@ class Vim < Formula
   sha256 "4ba0e6e099c4a6565e1c510ef3a26e452eed1b3ff8a8e02ada9b66c164da785e"
   head "https://github.com/vim/vim.git"
 
-  bottle do
-    sha256 "8655aaa0b9cb558bb5a7d3b64ba1332ce6af7a761d884d4373ddf2b53a934329" => :mojave
-    sha256 "258a9ea36248d39901a2fca7a281f4449d20d6bd11cd3eca1291cbacf8595483" => :high_sierra
-    sha256 "5c8a3c1d8fd3131a74805491f1e6c2c412670bf043bfcb8f7fff35465cb80540" => :sierra
-  end
+#   bottle do
+#     sha256 "8655aaa0b9cb558bb5a7d3b64ba1332ce6af7a761d884d4373ddf2b53a934329" => :mojave
+#     sha256 "258a9ea36248d39901a2fca7a281f4449d20d6bd11cd3eca1291cbacf8595483" => :high_sierra
+#     sha256 "5c8a3c1d8fd3131a74805491f1e6c2c412670bf043bfcb8f7fff35465cb80540" => :sierra
+#   end
 
   option "with-override-system-vi", "Override system vi"
   option "with-gettext", "Build vim with National Language Support (translated messages, keymaps)"
