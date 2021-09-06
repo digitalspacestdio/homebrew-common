5adcd7b4e27b049d872f540afe253e2dd8e63c8a
scherepanov <sergey@cherepanov.org.ua>
2021-09-06 15:59:26 +0400

    fix elasticsearch

--- Formula/elasticsearch@7.6.rb
+++ Formula/elasticsearch@7.6.rb
@@ -80,35 +80,12 @@ class ElasticsearchAT76 < Formula
   end
 
   plist_options :manual => "elasticsearch"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>KeepAlive</key>
-          <false/>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/elasticsearch</string>
-          </array>
-          <key>EnvironmentVariables</key>
-          <dict>
-          </dict>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WorkingDirectory</key>
-          <string>#{var}</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/elasticsearch.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/elasticsearch.log</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_sbin/"elasticsearch"]
+    run_type :immediate
+    keep_alive true
+    error_log_path var/"log/elasticsearch.log"
+    working_dir var
   end
 
   test do
