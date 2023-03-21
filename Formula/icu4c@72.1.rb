badc3f2b783ffd113ee521e711753cf448143094
scherepanov <sergey@cherepanov.org.ua>
2023-03-21 17:12:43 +0000

    icu4c@72.1.rb

---
+++ Formula/icu4c@72.1.rb
@@ -0,0 +1,43 @@
+class Icu4cAT721 < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "http://site.icu-project.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-72-1/icu4c-72_1-src.tgz"
+  version "72.1"
+  sha256 "a2d2d38217092a7ed56635e34467f92f976b370e20182ad325edea6681a71d68"
+  license "ICU"
+
+  livecheck do
+    url :stable
+    regex(/^release[._-]v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.gsub("-", ".") }.compact
+    end
+  end
+
+  keg_only :versioned_formula
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
+
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    if File.exist? "/usr/share/dict/words"
+      system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+    else
+      (testpath/"hello").write "hello\nworld\n"
+      system "#{bin}/gendict", "--uchars", "hello", "dict"
+    end
+  end
+end
