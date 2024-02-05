5a3884535be8eec886715a82e65c3f30d6681496
scherepanov <sergey@cherepanov.org.ua>
2024-02-06 01:34:16 +0400

    bottles update

--- bin/_common-bottles-upload.sh
+++ bin/_common-bottles-upload.sh
@@ -8,7 +8,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 brew tap digitalspacestdio/common
 brew tap digitalspacestdio/common
-cd $(brew tap-info --json digitalspacestdio/common | jq -r '.[].path')
+cd $(brew tap-info --json digitalspacestdio/common | jq -r '.[].path' | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;')
 #git stash
 #git pull
 
@@ -21,9 +21,9 @@ for FORMULA in $FORMULAS; do
     ls | grep $FORMULA'.*--.*.json$' | awk -F'--' '{ print $0 " " $1 "-" $2 }' | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf '--no-run-if-empty'; fi;) -I{} bash -c 'mv {}'
     for jsonfile in ./*.json; do
         jsonfile=$(basename $jsonfile)
-        JSON_FORMULA_NAME=$(jq -r '.[].formula.name' "$jsonfile")
+        JSON_FORMULA_NAME=$(jq -r '.[].formula.name' "$jsonfile" | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;')
         if ! [[ -z $JSON_FORMULA_NAME ]]; then
-            mergedfile=$(jq -r '.["digitalspacestdio/common/'$JSON_FORMULA_NAME'"].formula.name + "-" + ."digitalspacestdio/common/'$JSON_FORMULA_NAME'".formula.pkg_version + ".json"' "$jsonfile")
+            mergedfile=$(jq -r '.["digitalspacestdio/common/'$JSON_FORMULA_NAME'"].formula.name + "-" + ."digitalspacestdio/common/'$JSON_FORMULA_NAME'".formula.pkg_version + ".json"' "$jsonfile" | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;')
             set -x
             while read tgzName; do
                 if [[ -f "$tgzName" ]]; then
@@ -32,11 +32,11 @@ for FORMULA in $FORMULAS; do
                     } || /usr/bin/true
                     s3cmd put "$tgzName" "s3://homebrew-bottles/$FORMULA/$tgzName"
                 fi
-            done < <(jq -r '."digitalspacestdio/common/'$JSON_FORMULA_NAME'".bottle.tags[].filename' "$jsonfile")
+            done < <(jq -r '."digitalspacestdio/common/'$JSON_FORMULA_NAME'".bottle.tags[].filename' "$jsonfile" | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;')
             set +x
             s3cmd info "s3://homebrew-bottles/$FORMULA/$mergedfile" >/dev/null && {
                 s3cmd get "s3://homebrew-bottles/$FORMULA/$mergedfile" "$mergedfile".src
-                if [[ "object" != $(cat "$mergedfile".src| jq -r type) ]]; then
+                if [[ "object" != $(cat "$mergedfile".src| jq -r type | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;') ]]; then
                     cp "$jsonfile" "$mergedfile".src
                 fi
                 jq -s  '.[1]."digitalspacestdio/common/'$JSON_FORMULA_NAME'".bottle.tags = .[0]."digitalspacestdio/common/'$JSON_FORMULA_NAME'".bottle.tags * .[1]."digitalspacestdio/common/'$JSON_FORMULA_NAME'".bottle.tags | .[1]' "$mergedfile".src "$jsonfile" > "$mergedfile"
@@ -51,7 +51,7 @@ for FORMULA in $FORMULAS; do
         fi
     done
 done
-cd $(brew tap-info --json digitalspacestdio/common | jq -r '.[].path')
+cd $(brew tap-info --json digitalspacestdio/common | jq -r '.[].path' | perl -pe 's/\+/\ /g;' -e 's/%(..)/chr(hex($1))/eg;')
 git add .
 git commit -m "bottles update"
 git pull --rebase
