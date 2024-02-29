750079b43527f492c717c6f394f901078ef195d2
scherepanov <sergey@cherepanov.org.ua>
2024-02-29 23:48:33 +0400

    ImageMagick-6.9.13-7

--- Formula/imagemagick6.rb
+++ Formula/imagemagick6.rb
@@ -4,17 +4,11 @@ class Imagemagick6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://www.imagemagick.org/download/ImageMagick-6.9.12-13.tar.xz"
-  sha256 "a72cb13e79a0878a6fd07f877a61e683c29b646d4fd3ca9ff1ddc042cae846c6"
+  url "https://www.imagemagick.org/download/ImageMagick-6.9.13-7.tar.xz"
+  sha256 "2a521f7992b3dd32469b7b7254a81df8a0045cb0b963372e3ba6404b0a4efeae"
   head "https://github.com/imagemagick/imagemagick6.git"
-  version "6.9.12"
-  revision 13
-
-#   bottle do
-#     sha256 "ef26aa5e74724de5ad4eced9fec645c118c4f1eb212dbda0f241e9189cc089db" => :mojave
-#     sha256 "34542d49f95afd743a5520e0a3e73526242b7d2aa2792ddaf1964ebb281e7ca9" => :high_sierra
-#     sha256 "4b266f15f9a57fd542dd3ee6ffdad845a9f61cd757b7c7c08f5b9d9c99a8fa35" => :sierra
-#   end
+  version "6.9.13"
+  revision 1
 
   keg_only :versioned_formula
 
