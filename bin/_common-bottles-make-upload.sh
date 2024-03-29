674c87fab2019594e23042a0bc36a6a86fcdda2e
scherepanov <sergey@cherepanov.org.ua>
2024-03-29 21:16:31 +0000

    versions

--- bin/_common-bottles-make-upload.sh
+++ bin/_common-bottles-make-upload.sh
@@ -4,7 +4,7 @@ if [[ ! -z $DEBUG ]]; then set -x; fi
 pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 cd "${DIR}"
 
-FORMULAS=$(brew search digitalspacestdio/common | grep "$1\|$1@.\+" | awk -F'/' '{ print $3 }' | sort)
+FORMULAS=${@:-$(brew search digitalspacestdio/common | awk -F'/' '{ print $3 }' | sort)}
 
 for FORMULA in $FORMULAS; do
     echo "---> Starting $FORMULA"
