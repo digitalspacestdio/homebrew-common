3e2624ce7f82f7662998a6afdaaa49528dae26cb
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-03-30 02:59:26 +0400

    bottles update

--- Formula/libpq@16.2-icu4c.72.1.rb
+++ Formula/libpq@16.2-icu4c.72.1.rb
@@ -13,6 +13,7 @@ class LibpqAT162Icu4c721 < Formula
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libpq@16.2-icu4c.72.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "cbc744499fa8a31969f837d1f274ee79df7d93685d6f901ac4fc3e9dccb3be4c"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "2b69ff0f90dff06e4289f5089bdd07d5900136b199575b59ef31ff1990ef7c38"
   end
 
--- Formula/libpq@16.2-icu4c.73.2.rb
+++ Formula/libpq@16.2-icu4c.73.2.rb
@@ -13,6 +13,7 @@ class LibpqAT162Icu4c732 < Formula
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libpq@16.2-icu4c.73.2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "203971f06ecc17f56c413d03f90b058668ffc6a926a45a0ee1c2f4bdbd89669e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4a034cb2397fa70c89e3dcf0bce43392a25e956f0d1f7a06a6bd29f8ace6feca"
   end
 
