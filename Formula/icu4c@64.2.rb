8154ddf7cc83a5551714a4cd8bda5755214b5751
scherepanov <sergey@cherepanov.org.ua>
2024-03-31 05:40:14 +0400

    bottles update: icu4c@69.1

--- Formula/icu4c@64.2.rb
+++ Formula/icu4c@64.2.rb
@@ -15,11 +15,11 @@ class Icu4cAT642 < Formula
 
   keg_only :versioned_formula
 
-  #depends_on "gcc@9" => :build if OS.mac?
+  #depends_on "gcc@11" => :build if OS.linux?
 
   def install
-    #ENV["CC"] = "#{Formula["gcc@9"].opt_prefix}/bin/gcc-9" if OS.mac?
-    #ENV["CXX"] = "#{Formula["gcc@9"].opt_prefix}/bin/g++-9" if OS.mac?
+    #ENV["CC"] = "#{Formula["gcc@11"].opt_prefix}/bin/gcc-11" if OS.linux?
+    #ENV["CXX"] = "#{Formula["gcc@11"].opt_prefix}/bin/g++-11" if OS.linux?
     args = %W[
       --prefix=#{prefix}
       --disable-samples
