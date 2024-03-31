b9e3ec6f00eddf796a65d07b9ad67cf8eb87b781
scherepanov <sergey@cherepanov.org.ua>
2024-03-31 19:49:34 +0400

    curl

--- Formula/curl@8.rb
+++ Formula/curl@8.rb
@@ -1,96 +1,96 @@
 class CurlAT8 < Formula
-    desc "Get a file from an HTTP, HTTPS or FTP server"
-    homepage "https://curl.se"
-    # Don't forget to update both instances of the version in the GitHub mirror URL.
-    url "https://curl.se/download/curl-8.7.1.tar.bz2"
-    mirror "https://github.com/curl/curl/releases/download/curl-8_7_1/curl-8.7.1.tar.bz2"
-    mirror "http://fresh-center.net/linux/www/curl-8.7.1.tar.bz2"
-    mirror "http://fresh-center.net/linux/www/legacy/curl-8.7.1.tar.bz2"
-    sha256 "05bbd2b698e9cfbab477c33aa5e99b4975501835a41b7ca6ca71de03d8849e76"
-    license "curl"
-  
-    livecheck do
-      url "https://curl.se/download/"
-      regex(/href=.*?curl[._-]v?(.*?)\.t/i)
-    end
-  
-    head do
-      url "https://github.com/curl/curl.git", branch: "master"
-  
-      depends_on "autoconf" => :build
-      depends_on "automake" => :build
-      depends_on "libtool" => :build
-    end
-  
-    keg_only :versioned_formula
-  
-    depends_on "pkg-config" => :build
-    depends_on "brotli"
-    depends_on "libidn2"
-    depends_on "libnghttp2"
-    depends_on "libssh2"
-    depends_on "openldap"
-    depends_on "openssl@3"
-    depends_on "rtmpdump"
-    depends_on "zstd"
-  
-    uses_from_macos "krb5"
-    uses_from_macos "zlib"
-  
-    # Fixes `curl: (23) Failed writing received data to disk/application`
-    # Remove in next release
-    patch do
-      url "https://github.com/curl/curl/commit/b30d694a027eb771c02a3db0dee0ca03ccab7377.patch?full_index=1"
-      sha256 "da4ae2efdf05169938c2631ba6e7bca45376f1d67abc305cf8b6a982c618df4d"
-    end
-  
-    def install
-      tag_name = "curl-#{version.to_s.tr(".", "_")}"
-      if build.stable? && stable.mirrors.grep(/github\.com/).first.exclude?(tag_name)
-        odie "Tag name #{tag_name} is not found in the GitHub mirror URL! " \
-             "Please make sure the URL is correct."
-      end
-  
-      system "./buildconf" if build.head?
-  
-      args = %W[
-        --disable-debug
-        --disable-dependency-tracking
-        --disable-silent-rules
-        --prefix=#{prefix}
-        --with-ssl=#{Formula["openssl@3"].opt_prefix}
-        --without-ca-bundle
-        --without-ca-path
-        --with-ca-fallback
-        --with-secure-transport
-        --with-default-ssl-backend=openssl
-        --with-libidn2
-        --with-librtmp
-        --with-libssh2
-        --without-libpsl
-      ]
-  
-      args << if OS.mac?
-        "--with-gssapi"
-      else
-        "--with-gssapi=#{Formula["krb5"].opt_prefix}"
-      end
-  
-      system "./configure", *args
-      system "make", "install"
-      system "make", "install", "-C", "scripts"
-      libexec.install "scripts/mk-ca-bundle.pl"
-    end
-  
-    test do
-      # Fetch the curl tarball and see that the checksum matches.
-      # This requires a network connection, but so does Homebrew in general.
-      filename = (testpath/"test.tar.gz")
-      system "#{bin}/curl", "-L", stable.url, "-o", filename
-      filename.verify_checksum stable.checksum
-  
-      system libexec/"mk-ca-bundle.pl", "test.pem"
-      assert_predicate testpath/"test.pem", :exist?
-      assert_predicate testpath/"certdata.txt", :exist?
-    end
+  desc "Get a file from an HTTP, HTTPS or FTP server"
+  homepage "https://curl.se"
+  # Don't forget to update both instances of the version in the GitHub mirror URL.
+  url "https://curl.se/download/curl-8.7.1.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_7_1/curl-8.7.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.7.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.7.1.tar.bz2"
+  sha256 "05bbd2b698e9cfbab477c33aa5e99b4975501835a41b7ca6ca71de03d8849e76"
+  license "curl"
+
+  livecheck do
+    url "https://curl.se/download/"
+    regex(/href=.*?curl[._-]v?(.*?)\.t/i)
   end
+
+  head do
+    url "https://github.com/curl/curl.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "brotli"
+  depends_on "libidn2"
+  depends_on "libnghttp2"
+  depends_on "libssh2"
+  depends_on "openldap"
+  depends_on "openssl@3"
+  depends_on "rtmpdump"
+  depends_on "zstd"
+
+  uses_from_macos "krb5"
+  uses_from_macos "zlib"
+
+  # Fixes `curl: (23) Failed writing received data to disk/application`
+  # Remove in next release
+  patch do
+    url "https://github.com/curl/curl/commit/b30d694a027eb771c02a3db0dee0ca03ccab7377.patch?full_index=1"
+    sha256 "da4ae2efdf05169938c2631ba6e7bca45376f1d67abc305cf8b6a982c618df4d"
+  end
+
+  def install
+    tag_name = "curl-#{version.to_s.tr(".", "_")}"
+    if build.stable? && stable.mirrors.grep(/github\.com/).first.exclude?(tag_name)
+      odie "Tag name #{tag_name} is not found in the GitHub mirror URL! " \
+            "Please make sure the URL is correct."
+    end
+
+    system "./buildconf" if build.head?
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-ssl=#{Formula["openssl@3"].opt_prefix}
+      --without-ca-bundle
+      --without-ca-path
+      --with-ca-fallback
+      --with-secure-transport
+      --with-default-ssl-backend=openssl
+      --with-libidn2
+      --with-librtmp
+      --with-libssh2
+      --without-libpsl
+    ]
+
+    args << if OS.mac?
+      "--with-gssapi"
+    else
+      "--with-gssapi=#{Formula["krb5"].opt_prefix}"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+    system "make", "install", "-C", "scripts"
+    libexec.install "scripts/mk-ca-bundle.pl"
+  end
+
+  test do
+    # Fetch the curl tarball and see that the checksum matches.
+    # This requires a network connection, but so does Homebrew in general.
+    filename = (testpath/"test.tar.gz")
+    system "#{bin}/curl", "-L", stable.url, "-o", filename
+    filename.verify_checksum stable.checksum
+
+    system libexec/"mk-ca-bundle.pl", "test.pem"
+    assert_predicate testpath/"test.pem", :exist?
+    assert_predicate testpath/"certdata.txt", :exist?
+  end
+end
