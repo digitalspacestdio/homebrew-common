afa0b8eca37abbd4e5eb508c4f42a18db7a9ed06
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-03-31 05:24:54 +0400

    bottles update: icu4c@67.1

--- Formula/icu4c@67.1.rb
+++ Formula/icu4c@67.1.rb
@@ -15,8 +15,10 @@ class Icu4cAT671 < Formula
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/icu4c@67.1"
-    sha256 cellar: :any_skip_relocation, sonoma:       "8e6acb5fb109a7cffd73e1cf5e119e451edeb324f0e9558234c183537c6b2ff1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1ea035c0d89deecc1e52c3e447319de462e69511ea3835c94c9eb1ed35b767e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7312a031c5ed6718f5da8cdc72bdcaff9563127351d1947b79a06bb53c45781e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "94e489d17505299bfbe82f5b451c0d624a00b34a18430d3bb4dd8c6a6304a82c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8e6acb5fb109a7cffd73e1cf5e119e451edeb324f0e9558234c183537c6b2ff1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ea035c0d89deecc1e52c3e447319de462e69511ea3835c94c9eb1ed35b767e6"
   end
 
   keg_only :versioned_formula
