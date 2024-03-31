b0770a6048fa7955e8dfff0a06f4de69fab5e4a1
scherepanov <sergey@cherepanov.org.ua>
2024-03-31 05:24:11 +0400

    bottles update: libxml2@2.12-icu4c.69.1 libxml2@2.12-icu4c.72.1 libxml2@2.12-icu4c.73.2 libxml2@2.12-icu4c.74.2 libxml2@2.9-icu4c.69.1

--- Formula/libxml2@2.12-icu4c.74.2.rb
+++ Formula/libxml2@2.12-icu4c.74.2.rb
@@ -15,6 +15,7 @@ class Libxml2AT212Icu4c742 < Formula
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libxml2@2.12-icu4c.74.2"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "76c16e8cef7795fb7f8ed9b6c2bfaa28094d1c1f8c381b6e5fddadbe03428937"
+    sha256 cellar: :any_skip_relocation, sonoma:       "fd2c163d950f39bc1c13c11828fd503c2e4545c02f8b2f9751897ac8caf51d22"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "feaa4e132f569f389a946d3f7b262fb179dbf5bf30f19c4abc80acc296743156"
   end
 
