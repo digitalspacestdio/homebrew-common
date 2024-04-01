ad9c50db792e7de006278088df042dd8b327912d
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-04-01 18:11:02 +0400

    bottles update: libiconv@1.15 libiconv@1.16

--- Formula/libiconv@1.15.rb
+++ Formula/libiconv@1.15.rb
@@ -7,8 +7,9 @@ class LibiconvAT115 < Formula
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libiconv@1.15"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "95f46552d0de80c02134cb9f74b3cae9ac85129ec19fbce1bec7210ca5b7528d"
-    sha256 cellar: :any_skip_relocation, sonoma:       "3f79564b4f35737308c103360b5a8a280422f39e31a61fc374b28c25631a581e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "95f46552d0de80c02134cb9f74b3cae9ac85129ec19fbce1bec7210ca5b7528d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f39f7b1df2197418114e8b862b68b5058e6b9267e91f86787e02aff0365eb8d2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3f79564b4f35737308c103360b5a8a280422f39e31a61fc374b28c25631a581e"
   end
 
   keg_only :versioned_formula
