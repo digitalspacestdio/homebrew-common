468f30a80506a158d1339cb28f1923a2404f2990
Serhii Cherepanov <scherepanov@Serhiis-MacBook-Pro.local>
2024-04-01 18:53:20 +0400

    bottles update: libxslt@1.10-icu4c.69.1 libxslt@1.10-icu4c.72.1 libxslt@1.10-icu4c.73.2 libxslt@1.10-icu4c.74.2

--- Formula/libxslt@1.10-icu4c.69.1.rb
+++ Formula/libxslt@1.10-icu4c.69.1.rb
@@ -14,9 +14,10 @@ class LibxsltAT110Icu4c691 < Formula
 
   bottle do
     root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libxslt@1.10-icu4c.69.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "fa57f6c3b6badc728d2923f3bba52aaca15372abce0b12a9361ef1b9039268cb"
-    sha256 cellar: :any_skip_relocation, sonoma:       "3b57d6dede0e15ded3b8cc84314158ed7d240c919e025f4b2a5c985cd369cc29"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "93bee195b091135a01e94a3b179e54b57571a92d7c74e7edf1635423ac4a590a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa57f6c3b6badc728d2923f3bba52aaca15372abce0b12a9361ef1b9039268cb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "8c94df319c5fcff56ada32bbd744d12ffebed5e877be6c4e2f2228721fdc7880"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3b57d6dede0e15ded3b8cc84314158ed7d240c919e025f4b2a5c985cd369cc29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93bee195b091135a01e94a3b179e54b57571a92d7c74e7edf1635423ac4a590a"
   end
   
     head do
