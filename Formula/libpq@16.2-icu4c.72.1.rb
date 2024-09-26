56222fcf9a43e9e7eb632b8b9dadd4ca4c5d4c69
scherepanov <s@cherepanov.co>
2024-09-26 16:54:22 +0400

    bottle libpq@16.2-icu4c.72.1 darwin22

--- Formula/libpq@16.2-icu4c.72.1.rb
+++ Formula/libpq@16.2-icu4c.72.1.rb
@@ -13,8 +13,9 @@ class LibpqAT162Icu4c721 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/libpq@16.2-icu4c.72.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "c2c17d7abfcce6df9590d60e60fa76f08b79d4d2f67b9e326adeec79b969ebe3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "12ceccb4dcc3c05b1de50bd543fd4f1edce010198268e92c1c8641e83a28367d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1edfbc713011390db02c597d4e7964c34de754bfb38bc291816d80e200ac7a0a"
+    sha256 cellar: :any_skip_relocation, ventura:       "c2c17d7abfcce6df9590d60e60fa76f08b79d4d2f67b9e326adeec79b969ebe3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12ceccb4dcc3c05b1de50bd543fd4f1edce010198268e92c1c8641e83a28367d"
   end
 
   keg_only "conflicts with postgres formula"
