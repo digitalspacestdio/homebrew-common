7fa5595699c733d41d721ebd1598a94f6d84bed5
scherepanov <s@cherepanov.co>
2024-09-26 10:46:48 +0000

    bottle icu4c@72.1 linux-gnu

--- Formula/icu4c@72.1.rb
+++ Formula/icu4c@72.1.rb
@@ -7,8 +7,8 @@ class Icu4cAT721 < Formula
   license "ICU"
 
   bottle do
-    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/0/icu4c@72.1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "86d944c40359f1b9c5b7ec6a061be32ec7f36e9313c4745847bda4f43af14002"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/icu4c@72.1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a8dffd53a10a73cb16fab1ecbd2a70a08532d621b90efa7315123419134aba18"
   end
 
   keg_only :versioned_formula
