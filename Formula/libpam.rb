acf36fb6a7d4d0eafb2d811ab6890f47f35b5b13
scherepanov <s@cherepanov.co>
2024-09-26 09:22:44 +0000

    cleanup

--- Formula/libpam.rb
+++ Formula/libpam.rb
@@ -1,14 +1,44 @@
 require "formula"
 
 class Libpam < Formula
-  desc "libpam"
+  desc "Pluggable Authentication Modules for Linux"
   homepage "https://github.com/linux-pam/linux-pam"
-  url "https://github.com/linux-pam/linux-pam/releases/download/v1.3.1/Linux-PAM-1.3.1.tar.xz"
-  sha256 "eff47a4ecd833fbf18de9686632a70ee8d0794b79aecb217ebd0ce11db4cd0db"
-  revision 1
+  url "https://github.com/linux-pam/linux-pam/releases/download/v1.6.1/Linux-PAM-1.6.1.tar.xz"
+  sha256 "f8923c740159052d719dbfc2a2f81942d68dd34fcaf61c706a02c9b80feeef8e"
+  license any_of: ["BSD-3-Clause", "GPL-1.0-only"]
+  head "https://github.com/linux-pam/linux-pam.git", branch: "master"
+
+  depends_on "pkg-config" => :build
+  depends_on "libnsl"
+  depends_on "libprelude"
+  depends_on "libtirpc"
+  depends_on "libxcrypt"
+  depends_on :linux
+
+  keg_only :versioned_formula
+
+  skip_clean :la
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--includedir=#{prefix}/include/security", "--enable-static-modules", "--disable-pie"
+    args = %W[
+      --disable-db
+      --disable-silent-rules
+      --disable-selinux
+      --includedir=#{include}/security
+      --oldincludedir=#{include}
+      --enable-securedir=#{lib}/security
+      --sysconfdir=#{etc}
+      --with-xml-catalog=#{etc}/xml/catalog
+      --with-libprelude-prefix=#{Formula["libprelude"].opt_prefix}
+    ]
+
+    system "./configure", *std_configure_args, *args
+    system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_match "Usage: #{sbin}/mkhomedir_helper <username>",
+                 shell_output("#{sbin}/mkhomedir_helper 2>&1", 14)
   end
 end
