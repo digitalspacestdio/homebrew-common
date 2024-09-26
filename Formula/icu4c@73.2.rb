0d18224b0200aa757bf6b07505d213118f2dfd3b
scherepanov <s@cherepanov.co>
2024-09-26 07:29:02 +0000

    bottles update

--- Formula/icu4c@73.2.rb
+++ Formula/icu4c@73.2.rb
@@ -7,13 +7,8 @@ class Icu4cAT732 < Formula
   license "ICU"
 
   bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/icu4c@73.2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "022c0aca4ee0b6afb04261f7d79c781a6daee6b5ae855fbccc273f2c126c6c88"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d94276c3231eb96c1569169c86c3086566f49c5cc7cd6d7d36fb8c61597399dc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "77f243d305d8df65f655c3285f5f2bc350adb8cb21339fa16c6a9f517a9d195a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ffb99550464e6ffb3f8330fe8aa38ea200a44b7259b640f9e0b46bf34115ac4d"
-    sha256 cellar: :any_skip_relocation, monterey:       "fc5e7013207abdeb2a19ac294bb78bdb6090a5240950416d0c20f1269be86d1d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb74f741bbeb48d69a99310804947311c2d348a6985814819292b6c6d0fd8890"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/0/icu4c@73.2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bc18025c5c4e6e223ef28fe88ffa86639a1e70fdcfd904f261d046b76252bafb"
   end
 
   keg_only :versioned_formula
