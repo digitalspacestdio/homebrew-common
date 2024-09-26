8b41cfae56ad8b8bb928b4ef8e5bf301f824c165
scherepanov <s@cherepanov.co>
2024-09-26 07:29:15 +0000

    bottles update

--- Formula/libxml2@2.12-icu4c.73.2.rb
+++ Formula/libxml2@2.12-icu4c.73.2.rb
@@ -13,13 +13,8 @@ class Libxml2AT212Icu4c732 < Formula
   end
 
   bottle do
-    root_url "https://f003.backblazeb2.com/file/homebrew-bottles/libxml2@2.12-icu4c.73.2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b8a10faeb6f669e29a038dd73b61ab4fd4cb5a11ce08f3ef1b77a64636b4928d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c1f1d2482e5f2ac659acb99b4be72b2c4a327f046128820c9d8a053754c23350"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6bf116d3c7e2c8bee1958a11930409b3a1905a236910dc982ecfaf64c49fe142"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5c8fb7d3d3debd7e1d3f194b8bab721efdbf8f58199193e5dafb4475a72ca827"
-    sha256 cellar: :any_skip_relocation, monterey:       "eb3e7f019a1f35c327bf9538c1fee63cadd8abe8ef4c6214c1f113b5b02fbdd5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "46d0101d8de01bb6ca8d000d34dcb70124380dda55a85c217d69acfc99f73648"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/0/libxml2@2.12-icu4c.73.2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e581ba6d6e7dbce70cab8f75ebf117d8d04f8fa9cfe31e36861de002c21150a9"
   end
 
   head do
