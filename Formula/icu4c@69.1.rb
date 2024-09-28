ad3b7c2fdfa916bd161bc27b5e07f4f9a455d0fc
s@cherepanov.co <s@cherepanov.co>
2024-09-28 23:28:17 +0400

    bottle icu4c@69.1 linux-gnu

--- Formula/icu4c@69.1.rb
+++ Formula/icu4c@69.1.rb
@@ -11,6 +11,7 @@ class Icu4cAT691 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "a77fe00396a02f4a0e2fe007c752d471bef2fc6e50f5dccac64377364b00bf82"
     sha256 cellar: :any_skip_relocation, ventura:       "20a39508badeb51fe2c0db722ed684d24947eb0611f776150d4e49f5cd48e7cb"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4fb4a8702c62503221e806d8f701e3d3e367858749c78573a92d966c710ace43"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "8c211f6ba7b797525d7844736d33ec8973e1a8471cb3a442e77b2fe1aa0e1ead"
   end
 
   keg_only :versioned_formula
