b0f81749856604dbe4f101e99f8f4ced025aeb6a
s@cherepanov.co <s@cherepanov.co>
2024-09-28 22:40:08 +0400

    bottle openssl111w linux-gnu

--- Formula/openssl111w.rb
+++ Formula/openssl111w.rb
@@ -15,7 +15,7 @@ class Openssl111w < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "626b97bdd40dd433664cb0b05458e137ddcb387d1aee76133668928ea3fc60c7"
     sha256 cellar: :any_skip_relocation, ventura:       "38b9b601ab22a2a88ba04014a1994f5916a589bceb9ba213b0b062bf176923d9"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "922fa6f8904f81cfa321f17a169b83d383e6746a4ee8176affc56ca82905577e"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "fbdeecd6dc2199afde61df56c202253e1780f21dadefcc416003adc86b4c376d"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "43f330148d0d59ea91fdb2ee3154752bdaf043f07c94e668039da6e2af4d88bc"
   end
 
   keg_only :versioned_formula
