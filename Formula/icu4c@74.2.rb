f8a1e71f3a0f43c3f0479a479e4cec61f0f42c91
scherepanov <s@cherepanov.co>
2025-02-15 22:52:15 +0400

    bottle icu4c@74.2 darwin22

--- Formula/icu4c@74.2.rb
+++ Formula/icu4c@74.2.rb
@@ -9,6 +9,7 @@ class Icu4cAT742 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/icu4c@74.2"
+    sha256 cellar: :any_skip_relocation, ventura:      "cdbaac0f52e20d927f6c636ed82ef3632ddfd53f4f823c47d351b0ce96d68d56"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "082191ec0de459f9e9db2e17b6c3ff6ffb357c91107b9eef7fa10d3b36c8b35a"
   end
 
