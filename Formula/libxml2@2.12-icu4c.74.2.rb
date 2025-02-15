ffacdad284139b3fb8877f6122abb9e18ba565eb
Workspace Owner <scherepanov@scherepanov-ubuntu-coder.cloudspace.local>
2025-02-15 18:37:48 +0000

    bottle libxml2@2.12-icu4c.74.2 linux-gnu

--- Formula/libxml2@2.12-icu4c.74.2.rb
+++ Formula/libxml2@2.12-icu4c.74.2.rb
@@ -16,8 +16,8 @@ class Libxml2AT212Icu4c742 < Formula
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/libxml2@2.12-icu4c.74.2"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "2bcf129831e985e8b615b22e505a959abf113e8f36459913a1ecceafea1fe33e"
     sha256 cellar: :any_skip_relocation, ventura:       "b980349a59a60b49cec1f31fb962040da69dc4b0205778e8e1f85997fe1ce398"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "96b129486ff19e0a2a01f8eab839c05a68611de1611205c77fe2f7935a26e271"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "907d8c559a4f64a91ce694bb0df9ab103503e3c9529a27c502d53014a69d456d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f94769f6fe958a12da7f397a23510bfcb4f700aa252ec1c241e392f5645a8115"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8e74e1595ab9fc8b0428bc882a843e2d28c3327d26dde8ca2df2bfa341772024"
   end
 
   head do
