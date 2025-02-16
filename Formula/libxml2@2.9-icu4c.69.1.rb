716fe850325e27ac7c0d0aa120d984001aab2dd3
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-17 01:16:23 +0400

    bottle libxml2@2.9-icu4c.69.1 darwin22

--- Formula/libxml2@2.9-icu4c.69.1.rb
+++ Formula/libxml2@2.9-icu4c.69.1.rb
@@ -8,8 +8,9 @@ class Libxml2AT29Icu4c691 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/libxml2@2.9-icu4c.69.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "67ba8e4c2b569b8b506a89004e71dcc26360fafe7419b48339933ac62577f2aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7961b1fbdc696d5ea5ce175e3221903ec4eca9fd1d0dc54a1e05ff71fe93549b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "53bf8952abeebc6211fb39ee7d22e02cd3b5f738ee9c2ddd4fa197f1e0f6d4d8"
+    sha256 cellar: :any_skip_relocation, ventura:       "67ba8e4c2b569b8b506a89004e71dcc26360fafe7419b48339933ac62577f2aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7961b1fbdc696d5ea5ce175e3221903ec4eca9fd1d0dc54a1e05ff71fe93549b"
   end
 
   head do
