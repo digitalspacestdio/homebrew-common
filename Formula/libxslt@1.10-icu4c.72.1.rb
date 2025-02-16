5cbf16099849ccbf78391ee997fb19c44250c662
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-17 01:17:19 +0400

    bottle libxslt@1.10-icu4c.72.1 darwin22

--- Formula/libxslt@1.10-icu4c.72.1.rb
+++ Formula/libxslt@1.10-icu4c.72.1.rb
@@ -15,8 +15,9 @@ class LibxsltAT110Icu4c721 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/libxslt@1.10-icu4c.72.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "c39624fb977c8773159ac322f278694929e6b24a93238aaa0469a4a71bf41301"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "49ed15e51b2b3afc1bf0cd87c7f4d93c8e91f79dec10827e2f9bc14500631044"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e38376b5e466ccd0e67fe8ee7fc1f3202a1f8824c7b743222d2a46ac7d6da7c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "c39624fb977c8773159ac322f278694929e6b24a93238aaa0469a4a71bf41301"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49ed15e51b2b3afc1bf0cd87c7f4d93c8e91f79dec10827e2f9bc14500631044"
   end
   
     head do
