6affefc869132f75477dbe0d2d2ecc89fc99d30d
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-17 00:42:55 +0400

    bottle icu4c@72.1 darwin22

--- Formula/icu4c@72.1.rb
+++ Formula/icu4c@72.1.rb
@@ -9,8 +9,9 @@ class Icu4cAT721 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/icu4c@72.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "5c75e35ab38adaba21298b8867e0dbbd60c0d14adae6314ded80945656d18245"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6b97727ed632438fcc83a8bbca1f515722aca14b5e637156d0c0ba84aa5c7e53"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a4fec0453b5642616e6b7788dc757860b5d46756e8296226ac53c233e5f87747"
+    sha256 cellar: :any_skip_relocation, ventura:       "5c75e35ab38adaba21298b8867e0dbbd60c0d14adae6314ded80945656d18245"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b97727ed632438fcc83a8bbca1f515722aca14b5e637156d0c0ba84aa5c7e53"
   end
 
   keg_only :versioned_formula
