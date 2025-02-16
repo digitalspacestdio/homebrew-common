0b687330418256788900d949d8856dd1deed30db
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-17 00:51:16 +0400

    bottle imagemagick7 darwin22

--- Formula/imagemagick7.rb
+++ Formula/imagemagick7.rb
@@ -14,8 +14,9 @@ class Imagemagick7 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/imagemagick7"
-    sha256 cellar: :any_skip_relocation, ventura:      "03dd5395c0168a99649682f4998e21359fbbe919ceb9d480cf6fb75e3fa59fee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1cb3b3c82309a82a06cb262225899b109b53a4d5bb354e09902736f4f3992541"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6b2f086014e3a3d2bdbd7f3399456a1de8fded68480a96bd1affba71f63a40d5"
+    sha256 cellar: :any_skip_relocation, ventura:       "03dd5395c0168a99649682f4998e21359fbbe919ceb9d480cf6fb75e3fa59fee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cb3b3c82309a82a06cb262225899b109b53a4d5bb354e09902736f4f3992541"
   end
 
   depends_on "pkg-config" => :build
