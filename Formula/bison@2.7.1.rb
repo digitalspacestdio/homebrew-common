42c004291c4b05dc0fde987f6f74a0d807ab8a67
scherepanov <s@cherepanov.co>
2025-02-18 16:00:35 +0400

    bottle bison@2.7.1 darwin22

--- Formula/bison@2.7.1.rb
+++ Formula/bison@2.7.1.rb
@@ -9,6 +9,7 @@ class BisonAT271 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/common/bison@2.7.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "51ebc7520fd777c2a71f33cd9608ecf9ac3a58fb50e117706510ea78a56a886c"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "53e026e421e9683293c511db0f171283c9f19848f20c9ceda655b48410712c96"
   end
   
